7b6382df75a533be48b8252428c5b98752691b79
Alfredo Quiroga <aquiroga@Alfredos-MacBook-Pro-2.local>
2016-01-07 12:51:19 -0500

    Moving import.sql to the root of the classpath
    - Made sure the User entity uses a "users" name for the table

--- src/main/java/com/redhat/openshift/spring/models/User.java
+++ src/main/java/com/redhat/openshift/spring/models/User.java
@@ -10,7 +10,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@Entity
+@Entity(name = "users")
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class User implements Serializable {
